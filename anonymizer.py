a6f84c02e538e602eec7305f5b9fb92a667adb13
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-04 17:20:49 -0700

    TODOs

--- anonymizer.py
+++ anonymizer.py
@@ -5,9 +5,9 @@ import random
 from reserved_words import sql_reserved_words
 
 longquery = """SELECT
-    emp.EmployeeID,
-    emp.FirstName,
-    emp.LastName,
+    info.EmployeeID,
+    info.FirstName,
+    info.LastName,
     dept.DepartmentName,
     pos.PositionTitle,
     COUNT(proj.ProjectID) AS NumberOfProjects,
@@ -15,23 +15,23 @@ longquery = """SELECT
     AVG(ts.Hours) AS AverageHoursPerProject,
     MAX(ts.Date) AS LastProjectDate
 FROM
-    Employees emp
+    Employees info
 JOIN
-    Departments dept ON emp.DepartmentID = dept.DepartmentID
+    Departments dept ON info.DepartmentID = dept.DepartmentID
 JOIN
-    Positions pos ON emp.PositionID = pos.PositionID
+    Positions pos ON info.PositionID = pos.PositionID
 LEFT JOIN
-    ProjectAssignments pa ON emp.EmployeeID = pa.EmployeeID
+    ProjectAssignments pa ON info.EmployeeID = pa.EmployeeID
 LEFT JOIN
     Projects proj ON pa.ProjectID = proj.ProjectID
 LEFT JOIN
     TimeSheets ts ON proj.ProjectID = ts.ProjectID
 WHERE
     dept.DepartmentName IN ('IT', 'Finance', 'Marketing')
-    AND emp.StartDate BETWEEN '2020-01-01' AND '2023-01-01'
+    AND info.StartDate BETWEEN '2020-01-01' AND '2023-01-01'
     AND ts.Date >= DATE_SUB(CURRENT_DATE, INTERVAL 1 YEAR)
 GROUP BY
-    emp.EmployeeID, dept.DepartmentName, pos.PositionTitle
+    info.EmployeeID, dept.DepartmentName, pos.PositionTitle
 HAVING
     TotalHours > 100
 ORDER BY
@@ -54,6 +54,11 @@ def convert_SQL(sql_query):
     def generate_random_string(length=8):
         # TODO the list should be constantly refreshed in the background the avoid the cost of processing
         # TODO the random strings should have null intersection with: sql reserved words, and ideally original tokens
+
+        # TODO can have "levels" of anonymization. For example, a table name as 'A' or common words like "data", "info"
+        #  etc probably don't need to be anonymized
+
+        # TODO even better here would be random words from the dictionary, without replacement
         letters = string.ascii_lowercase
         return ''.join(random.choice(letters) for i in range(length))
 
@@ -64,7 +69,8 @@ def convert_SQL(sql_query):
         # tokens1 = re.split(r"(\s+|,|;|\(|\))", query)
         for i, token in enumerate(tokens):
             if token.upper() not in sql_reserved_words_upper and re.match(r'\w+', token):
-                d[token] = generate_random_string()
+                if token not in d:
+                    d[token] = generate_random_string()
                 # TODO there MUST be a more efficient way where we go through only once instead of <number of tokens>
                 #  times, especially since we can hash the tokens and find they quickly. The only challenge is that as
                 #  we go throught the query, we have to know where tokens start and end
@@ -76,20 +82,22 @@ def convert_SQL(sql_query):
     return new_query, d
 
 
-
 def unanonymize(query, d):
     for original_token, sanitized_token in d.items():
         query = replace_in_string(token=sanitized_token, replacement=original_token, string=query)
     return query
 
 
-# print(convert_SQL(longquery))  # TODO
 
 def test_quick(query):
+    print("Original:")
     print(query)
     res1, d = convert_SQL(query)
+    print("\nAnon:")
     print(res1)
     unres1 = unanonymize(res1, d=d)
+    print("\nRestored:")
     print(unres1)
 
-test_quick("SELECT EmployeeID, LastName FROM Employees WHERE Department = 'Sales';")
+# test_quick("SELECT EmployeeID, LastName FROM Employees WHERE Department = 'Sales';")
+test_quick(longquery)
